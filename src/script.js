91f816282505591b86ee19e0dc645d05d0eb9408
Zynno-Dev <michelinbriandev@gmail.com>
2021-05-20 08:51:48 -0300

    Finish him!

--- src/script.js
+++ src/script.js
@@ -3,6 +3,9 @@ $("#tarjetaUno").hide();
 $("#tarjetaDos").hide();
 $("#tarjetaTres").hide();
 
+let TarjetaUno = false;
+let TarjetaDos = false;
+let TarjetaTres = false;
 
 setTimeout(
     function(){
@@ -20,16 +23,39 @@ $("#productoUno").click(function (e) {
     e.preventDefault();
     $("#productosBlock").hide("slow");
     $("#tarjetaUno").show("slow");
+    TarjetaUno = true;
 });
 
 $("#productoDos").click(function (e) { 
     e.preventDefault();
     $("#productosBlock").hide("slow");
     $("#tarjetaDos").show("slow");
+    TarjetaDos = true;
 });
 
 $("#productoTres").click(function (e) { 
     e.preventDefault();
     $("#productosBlock").hide("slow");
     $("#tarjetaTres").show("slow");
+    TarjetaTres = true;
+});
+
+$(".buttonBack").click(function (e) { 
+    e.preventDefault();
+    if (TarjetaUno){
+        $("#tarjetaUno").hide("slow");
+        $("#productosBlock").show("slow");
+        TarjetaUno = false
+    }
+    else if (TarjetaDos){
+        $("#tarjetaDos").hide("slow");
+        $("#productosBlock").show("slow");
+        TarjetaDos = false
+    }
+    else if (TarjetaTres){
+        $("#tarjetaTres").hide("slow");
+        $("#productosBlock").show("slow");
+        TarjetaTres = false
+    }
+    
 });
